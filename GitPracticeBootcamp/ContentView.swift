8ff3d1691f85760f90e4e74bc6b007738fad7f4e
Christian Francis <cfrancis@archerguards.com>
2024-11-20 09:41:03 -0500

    First commit

--- GitPracticeBootcamp/ContentView.swift
+++ GitPracticeBootcamp/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Welcome")
+            Text("Welcome to my app!")
         }
         .padding()
     }
