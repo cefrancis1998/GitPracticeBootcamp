65e6c5c3bf95062b705462d4599e8756c6e00113
Christian Francis <cfrancis@archerguards.com>
2024-11-20 09:29:41 -0500

    Initial Commit

---
+++ GitPracticeBootcamp/GitPracticeBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeBootcampApp.swift
+//  GitPracticeBootcamp
+//
+//  Created by Christian Francis on 2024-11-20.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
